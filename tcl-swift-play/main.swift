13d4e735e09788b643e58ffea2787add41058b4c
Karl Lehenbauer <karl@flightaware.com>
2016-04-06 13:14:29 -0500

    Initial Commit

---
+++ tcl-swift-play/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  tcl-swift-play
+//
+//  Created by Karl Lehenbauer on 4/6/16.
+//  Copyright © 2016 FlightAware. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
