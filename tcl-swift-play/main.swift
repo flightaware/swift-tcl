0e49235fe9d93fc1b9757703bdb46230340f2f72
Karl Lehenbauer <karl@flightaware.com>
2016-04-06 21:45:16 -0500

    move tcl swift class to its own file

--- tcl-swift-play/main.swift
+++ tcl-swift-play/main.swift
@@ -10,65 +10,6 @@ import Foundation
 
 print("Hello, World!")
 
-class TclInterp {
-    var interp: UnsafeMutablePointer<Tcl_Interp>
-    
-    init() {
-        interp = Tcl_CreateInterp()
-        Tcl_Init(interp)
-    }
-    
-    deinit {
-        Tcl_DeleteInterp (interp)
-    }
-    
-    func eval(code: String) {
-        let ret = Tcl_Eval(interp, code.cStringUsingEncoding(NSUTF8StringEncoding)!)
-        
-        print("eval return code is \(ret)")
-    }
-    
-    func resultString() -> String {
-        return(String.fromCString(Tcl_GetString(Tcl_GetObjResult(interp))))!
-    }
-}
-
-class TclObj {
-    var obj: UnsafeMutablePointer<Tcl_Obj>
-    
-    init() {
-        obj = Tcl_NewObj()
-    }
-    
-    init(val: Int) {
-        obj = Tcl_NewLongObj(val)
-    }
-    
-    init(val: String) {
-        obj = Tcl_NewStringObj (val.cStringUsingEncoding(NSUTF8StringEncoding)!, -1)
-    }
-    
-    init(val: Double) {
-        obj = Tcl_NewDoubleObj (val)
-    }
-    
-    deinit {
-        DecrRefCount(obj)
-    }
-    
-    func string() -> String {
-        return(String.fromCString(Tcl_GetString(obj)))!
-    }
-    
-    func int() -> Int? {
-        var longVal: CLong = 0
-        let result = Tcl_GetLongFromObj (nil, obj, &longVal)
-        if (result == TCL_ERROR) {
-            return nil
-        }
-        return longVal
-    }
-}
 
 let interp = TclInterp()
 
---
+++ tcl-swift-play/tcl.swift
@@ -0,0 +1,69 @@
+//
+//  tcl.swift
+//  tcl-swift-play
+//
+//  Created by Karl Lehenbauer on 4/6/16.
+//  Copyright © 2016 FlightAware. All rights reserved.
+//
+
+import Foundation
+
+class TclInterp {
+    var interp: UnsafeMutablePointer<Tcl_Interp>
+    
+    init() {
+        interp = Tcl_CreateInterp()
+        Tcl_Init(interp)
+    }
+    
+    deinit {
+        Tcl_DeleteInterp (interp)
+    }
+    
+    func eval(code: String) {
+        let ret = Tcl_Eval(interp, code.cStringUsingEncoding(NSUTF8StringEncoding)!)
+        
+        print("eval return code is \(ret)")
+    }
+    
+    func resultString() -> String {
+        return(String.fromCString(Tcl_GetString(Tcl_GetObjResult(interp))))!
+    }
+}
+
+class TclObj {
+    var obj: UnsafeMutablePointer<Tcl_Obj>
+    
+    init() {
+        obj = Tcl_NewObj()
+    }
+    
+    init(val: Int) {
+        obj = Tcl_NewLongObj(val)
+    }
+    
+    init(val: String) {
+        obj = Tcl_NewStringObj (val.cStringUsingEncoding(NSUTF8StringEncoding)!, -1)
+    }
+    
+    init(val: Double) {
+        obj = Tcl_NewDoubleObj (val)
+    }
+    
+    deinit {
+        DecrRefCount(obj)
+    }
+    
+    func string() -> String {
+        return(String.fromCString(Tcl_GetString(obj)))!
+    }
+    
+    func int() -> Int? {
+        var longVal: CLong = 0
+        let result = Tcl_GetLongFromObj (nil, obj, &longVal)
+        if (result == TCL_ERROR) {
+            return nil
+        }
+        return longVal
+    }
+}
