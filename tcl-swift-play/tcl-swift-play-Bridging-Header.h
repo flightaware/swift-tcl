3faf0ea5b7d6fd620ddf81895514ec63602c05b0
Peter da Silva <resuna@gmail.com>
2017-07-27 13:55:05 -0500

    Clean up last bits of old tree

--- tcl-swift-play/tcl-swift-play-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "/opt/local/include/tcl.h"
-
-void DecrRefCount(Tcl_Obj *obj);
-
-void IncrRefCount(Tcl_Obj *obj);
